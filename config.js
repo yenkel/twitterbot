bd516e81c8452d17b50c3f98bab12cad60763025
“yenkelgit config --global user.namgit config --global user.name “yenkel <yenkel.mayost@gmail.com>
2017-04-25 10:12:46 +0300

    tokens

---
+++ config.js
@@ -0,0 +1,13 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+    consumer_key: 'nAz8Ytw15mUhTbrM08P5wKf83',
+    consumer_secret: 'txESef7uMoFfCs6Rs9nDFc8fz91RNLQ6qAFQaOFzwK527PAmRm',
+    access_token: '856762306822275072-wK0mDhYoXLu2SfbRmwpCmYxUgWsiMyo',
+    access_token_secret: 'jyxFycTMQg1GjzQfDba7e9iQ4sWQawzd6icNt02BYS5cZ'
+}
